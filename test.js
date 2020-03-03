53fbbcb3cb7bd48c5ac7a73d32ce697b1b770086
jeongsooyoung <code.ameba@gmail.com>
2020-03-03 18:47:02 +0900

    number of pythagoras

--- test.js
+++ test.js
@@ -191,3 +191,15 @@ for (let i = 1, sum = 0; i <= 10; i++) {
   sum += i;
   console.log(sum);
 }
+
+// test 21
+function sumArray(a) {
+  let sum = 0;
+  for (let i = 0; i < a.length; i++) {
+    sum += a[i];
+  }
+  return sum;
+}
+
+const a = [3, 5, 1, 2, 6, 7];
+console.log(sumArray(a)); // 24
