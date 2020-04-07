581874ae3e6faf765fd9cdeff8f53e7321d64660
jeongsooyoung <code.ameba@gmail.com>
2020-04-07 17:39:20 +0900

    whale talk

---
+++ whaletalk/main.js
@@ -0,0 +1,27 @@
+const input = 'hello human';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+      if (input[i] === 'e' || input[i] === 'u') {
+          resultArray.push(input[i]);
+       };
+    };
+  };
+};
+
+console.log((resultArray.join('')).toUpperCase());
+
+/*
+Whale Talk
+Take a phrase like ‘turpentine and turtles’ and translate it into its “whale talk” equivalent: ‘UUEEIEEAUUEE’.
+
+There are a few simple rules for translating text to whale language:
+
+There are no consonants. Only vowels excluding “y”.
+The u‘s and e‘s are extra long, so we must double them in our program.
+Once we have converted text to the whale language, the result is sung slowly, as is a custom in the ocean.
+*/
