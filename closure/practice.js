c466d3efb9e2f533b73dd0009a456f14ec2c1b83
jeongsooyoung <code.ameba@gmail.com>
2020-05-04 18:25:01 +0900

    practice closure

--- closure/practice.js
+++ closure/practice.js
@@ -1,6 +1,23 @@
-let globalFunc;
-{
-  let blockVar = 'Hello';
-  globalFunc = () => {console.log(blockVar)};
+// nested function
+function sayHiBye(firstName, lastName) {
+  function getFullName() {
+    return firstName + ' ' + lastName;
+  }
+  console.log('Hello, ' + getFullName());
+  console.log('Bye, ' + getFullName());
 }
-globalFunc();
+
+sayHiBye('John', 'Johnson');
+
+
+function makeCounter() {
+  let count = 0;
+  return function() {
+    return count++;
+  };
+}
+
+let counter = makeCounter();
+console.log(counter());
+console.log(counter());
+console.log(counter());
