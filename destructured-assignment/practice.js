b2bcf5cc23528a6b353c66e65e427ac4dd6f851a
jeongsooyoung <code.ameba@gmail.com>
2020-05-17 18:49:05 +0900

    practice destructured assignment

--- destructured-assignment/practice.js
+++ destructured-assignment/practice.js
@@ -23,3 +23,19 @@ let option = {
 
 let {width: w = 100, height: h = 200, title} = option;
 console.log(title, w, h);
+
+
+var array, first, second;
+array = [first, second] = [1, 2, 3, 4];
+console.log('first : ' + first, 'second : ' + second, 'array : ' + array); // first : 1 second : 2 array : 1,2,3,4
+
+// ...(spread operator)를 통해 남는 값을 모두 rest에 배열로 할당
+var [a, b, ...rest] = [1, 2, 3, 4];
+console.log(a, b, rest);
+
+// iterable object destructuring
+function* createNumber(from, to) {
+  while (from <= to) yield from++;
+}
+var [a, b, c, d, e] = createNumber(10, 15);
+console.log(a, b, c, d, e);
