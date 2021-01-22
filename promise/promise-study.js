daa6d478ff6b749fd058f1e677f2ce389d09d2c6
jeongsooyoung <code.ameba@gmail.com>
2021-01-22 23:42:11 +0900

    promise study

---
+++ promise/promise-study.js
@@ -0,0 +1,9 @@
+const promise = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log('A');
+    resolve();
+  }, 1000);
+});
+promise.then(function () {
+  console.log('B');
+});
